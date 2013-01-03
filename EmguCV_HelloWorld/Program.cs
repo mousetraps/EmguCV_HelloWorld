3616888f8e127d5b882554ff0bc9703150ae059e
Sara Itani <itani@mit.edu>
2013-01-02 23:41:27 -0500

    EmguCV working on Windows 8 RP, 64-bit

---
+++ EmguCV_HelloWorld/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using CvEnum = Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Drawing;
+
+
+namespace EmguCV_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The name of the window
+            String win1 = "Test Window";
+
+            //Create the window using the specific name
+            CvInvoke.cvNamedWindow(win1);
+
+            //Create an image of 400x200 of Blue color
+            using (Image<Bgr, Byte> img = new Image<Bgr, byte>(400, 200, new Bgr(255, 0, 0)))
+            {
+                //Create the font
+                MCvFont f = new MCvFont(CvEnum.FONT.CV_FONT_HERSHEY_COMPLEX, 1.0, 1.0);
+                //Draw "Hello, world." on the image using the specific font
+                img.Draw("Hello, world", ref f, new Point(10, 80), new Bgr(0, 255, 0));
+
+                //Show the image
+                CvInvoke.cvShowImage(win1, img.Ptr);
+                //Wait for the key pressing event
+                CvInvoke.cvWaitKey(0);
+                //Destory the window
+                CvInvoke.cvDestroyWindow(win1);
+            }
+        }
+    }
+}
